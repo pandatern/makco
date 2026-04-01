a7d8186dc837b2b8abb5915cabdc561c4aa0f5d2
TANK <tank@pandatern.tech>
2026-04-01 09:54:42 +0000

    Fix: move Juspay Maven repo to settings.gradle.kts

--- frontend/android/settings.gradle.kts
+++ frontend/android/settings.gradle.kts
@@ -11,6 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://repo.hyper-sdk.in/repository/hyper-sdk/") }
     }
 }
 
