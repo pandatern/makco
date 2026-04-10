93477cf0f78f33fc5216d656cb0315b13ef817b0
TANK <tank@pandatern.tech>
2026-04-10 02:27:36 -0400

    fix: StationPicker imports

--- frontend/android/app/src/main/java/com/pandatern/makco/ui/screens/StationPicker.kt
+++ frontend/android/app/src/main/java/com/pandatern/makco/ui/screens/StationPicker.kt
@@ -14,7 +14,10 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.pandatern.makco.R
