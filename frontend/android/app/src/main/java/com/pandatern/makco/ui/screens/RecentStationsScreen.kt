37c6fe1a756d575952d130f289a6390b85c3af9c
Baymax <baymax@pandatern.tech>
2026-04-12 07:39:23 -0400

    fix: add painterResource import

--- frontend/android/app/src/main/java/com/pandatern/makco/ui/screens/RecentStationsScreen.kt
+++ frontend/android/app/src/main/java/com/pandatern/makco/ui/screens/RecentStationsScreen.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.pandatern.makco.R
